ce5a41b9c80f3959904fad342d1c6439379c4a57
Miguel Malton <miguel.malton@gmail.com>
2025-09-03 14:59:23 -0300

    Criação do Dto ClientRequest e seu parametros

---
+++ src/main/java/miguel/dev/E_commerce/dto/ClientResquest.java
@@ -0,0 +1,8 @@
+package miguel.dev.E_commerce.dto;
+
+public class ClientResquest {
+    private String nome;
+    private String email;
+    private String senha;
+    private String cep;
+}
--- src/main/java/miguel/dev/E_commerce/model/Cliente.java
+++ src/main/java/miguel/dev/E_commerce/model/Cliente.java
@@ -20,6 +20,7 @@ public class Cliente {
     private Long id;
 
     private String nome;
+    private String email;
     private String senha;
     private String endereço;
     private String logradouro;
